eac7e51648a2d6da913a708358918327c15918fa
Steffen Engel <steffen.engel@nea-x.de>
2020-04-08 12:30:22 +0200

    removed gson

--- src/main/java/com/amazon/kinesis/kafka/DataUtility.java
+++ src/main/java/com/amazon/kinesis/kafka/DataUtility.java
@@ -1,5 +1,9 @@
 package com.amazon.kinesis.kafka;
 
+import com.amazonaws.services.kinesisfirehose.model.Record;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import java.io.UnsupportedEncodingException;
 import java.nio.Buffer;
 import java.nio.ByteBuffer;
@@ -7,15 +11,12 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import com.google.gson.Gson;
 
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.Schema.Type;
 import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.errors.DataException;
 import org.apache.kafka.connect.sink.SinkRecord;
-
-import com.amazonaws.services.kinesisfirehose.model.Record;
 
 public class DataUtility {
 
@@ -114,42 +115,44 @@ public class DataUtility {
 	 *            - Value of the message
 	 * @return Parsed bytebuffer as per schema type
 	 */
-	public static ByteBuffer parseValue(Object value) {
-        if (value == null) {
+        public static ByteBuffer parseValue(Object value) {
+            if (value == null) {
+                return null;
+            }
+            if (value instanceof Number) {
+                ByteBuffer longBuf = ByteBuffer.allocate(8);
+                longBuf.putLong((Long) value);
+                return longBuf;
+            }
+            if (value instanceof Boolean) {
+                ByteBuffer boolBuffer = ByteBuffer.allocate(1);
+                boolBuffer.put((byte) ((Boolean) value ? 1 : 0));
+                return boolBuffer;
+            }
+            if (value instanceof String) {
+                try {
+                    return ByteBuffer.wrap(((String) value).getBytes("UTF-8"));
+                } catch (UnsupportedEncodingException e) {
+                    System.out.println("Message cannot be translated:" + e.getLocalizedMessage());
+                }
+            }
+            if(value instanceof HashMap) {		
+                try {
+                    String json =  new ObjectMapper().writeValueAsString(value); 
+                    return parseValue(json);
+                } catch (JsonProcessingException e) {
+                    System.out.println("JSON couldn't be processed" + e.getLocalizedMessage());
+                }
+            }
+            if (value instanceof byte[] || value instanceof ByteBuffer) {
+                if (value instanceof byte[])
+                    return ByteBuffer.wrap((byte[]) value);
+                else if (value instanceof ByteBuffer)
+                    return (ByteBuffer) value;
+            }
+            System.out.println("Unsupported value type: " + value.getClass());
             return null;
         }
-        if (value instanceof Number) {
-            ByteBuffer longBuf = ByteBuffer.allocate(8);
-			longBuf.putLong((Long) value);
-			return longBuf;
-        }
-        if (value instanceof Boolean) {
-            ByteBuffer boolBuffer = ByteBuffer.allocate(1);
-			boolBuffer.put((byte) ((Boolean) value ? 1 : 0));
-			return boolBuffer;
-        }
-        if (value instanceof String) {
-            try {
-				return ByteBuffer.wrap(((String) value).getBytes("UTF-8"));
-			} catch (UnsupportedEncodingException e) {
-				System.out.println("Message cannot be translated:" + e.getLocalizedMessage());
-			}
-		}
-		if(value instanceof HashMap) {		
-			Gson gson = new Gson();
-			
-			String json = gson.toJson(value); 
-			return parseValue(json);
-		}
-        if (value instanceof byte[] || value instanceof ByteBuffer) {
-            if (value instanceof byte[])
-				return ByteBuffer.wrap((byte[]) value);
-			else if (value instanceof ByteBuffer)
-				return (ByteBuffer) value;
-        }
-		System.out.println("Unsupported value type: " + value.getClass());
-		return null;
-	}
 
 	/**
 	 * Converts Kafka record into Kinesis record
